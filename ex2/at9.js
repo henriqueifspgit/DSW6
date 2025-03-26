214ae7f8f4603afc1337f494191b0b64b86d62eb
carlos henrique <henrique.h@aluno.ifsp.edu.br>
2025-03-26 19:32:07 -0300

    exercicio 2 finalizado

---
+++ ex2/at9.js
@@ -0,0 +1,8 @@
+/* Atividade 9. Escreva um loop que realiza sete chamadas para console.log para exibir o seguinte triângulo: */
+
+let linha = ""; 
+
+for (let i = 0; i < 7; i++) {
+    linha += "#"; 
+    console.log(linha);
+}
